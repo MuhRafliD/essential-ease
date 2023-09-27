d623e22f4faf420f9849ed214ff8c433ab416ac2
MuhRafliD <mrafli1130@gmail.com>
2023-09-27 10:07:08 +0700

    menyelesaikan tugas 4

--- main/views.py
+++ main/views.py
@@ -5,16 +5,43 @@ from django.urls import reverse
 from main.models import Item
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
+@login_required(login_url='/login')
 def show_main(request):
-    item = Item.objects.all()
-    item_count = item.count()
+    item = Item.objects.filter(user=request.user)
+    
+    if request.method == 'POST':
+        if 'increment' in request.POST:
+            item_id = request.POST.get('increment')
+            items = item.get(id=item_id)
+            items.amount += 1
+            items.save()
+            return HttpResponseRedirect(reverse('main:show_main'))
+        elif 'decrement' in request.POST:
+            item_id = request.POST.get('decrement')
+            items = item.get(id=item_id)
+            items.amount -= 1
+            items.save()
+            return HttpResponseRedirect(reverse('main:show_main'))
+        elif 'delete' in request.POST:
+            item_id = request.POST.get('delete')
+            items = item.get(id=item_id)
+            items.delete()
+            return HttpResponseRedirect(reverse('main:show_main'))
 
+    total_items = item.count()
     context = {
-        'name': 'Muhammad Rafli Darmawan',
-        'class': 'PBP B',
-        'item': item,
-        'item_count': item_count
+        'name': request.user.username,
+        'class': "PBP B",
+        'items': item,
+        'item_count': total_items,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -23,8 +50,10 @@ def create_item(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
-        return HttpResponseRedirect(reverse('main:show_main'))
+     item = form.save(commit=False)
+     item.user = request.user
+     item.save()
+     return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
     return render(request, "create_item.html", context)
@@ -44,3 +73,36 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
